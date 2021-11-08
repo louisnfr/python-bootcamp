a681fb65d4fc78575186c20523618a4b3aac4ca9
louisnfr <louis.raffin@gmail.com>
2021-11-08 17:54:21 +0100

    m00e06

--- module00/ex05/kata00.py
+++ module00/ex05/kata00.py
@@ -1,4 +1,4 @@
-t = (19,21,42, 465)
+t = (19,21,42,465)
 
 delimiter = ', '
 str = delimiter.join([str(value) for value in t])
---
+++ module00/ex06/recipe.py
@@ -0,0 +1,10 @@
+cookbook = {
+'sandwich': {'ingredients': {'ham', 'bread', 'cheese', 'tomatoes'}, 'meal': 'lunch', 'prep_time': 10},
+'cake': {'ingredients': {'flour', 'sugar', 'eggs'}, 'meal': 'dessert', 'prep_time': 60},
+'salad': {'ingredients': {'avocado', 'arugula', 'tomatoes', 'spinach'}, 'meal': 'lunch', 'prep_time': 10},
+}
+
+def print_recipe(recipe):
+	print(cookbook.recipe)
+
+print_recipe(sandwich)
