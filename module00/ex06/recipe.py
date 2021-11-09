2e62e61fb9ae851986ba41775d08e8b6e7e5595e
louisnfr <louis.raffin@gmail.com>
2021-11-09 16:58:27 +0100

    m00ex06

--- module00/ex06/recipe.py
+++ module00/ex06/recipe.py
@@ -4,7 +4,14 @@ cookbook = {
 'salad': {'ingredients': {'avocado', 'arugula', 'tomatoes', 'spinach'}, 'meal': 'lunch', 'prep_time': 10},
 }
 
-def print_recipe(recipe):
-	print(cookbook.recipe)
 
-print_recipe(sandwich)
+def print_recipe(recipe):
+	print("recipe for ", recipe, ':', sep="")
+	print("ingredients:", cookbook[recipe]['ingredients'])
+	print("to be eaten for", cookbook[recipe]['meal'])
+	print("takes", cookbook[recipe]['prep_time'], "minutes to make")
+
+#def add_recipe(name, ingredients, meal, prep_time)
+
+print_recipe("sandwich")
+
